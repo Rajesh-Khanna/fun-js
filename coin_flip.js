f2c752f80be3f22bb799fd7832db3b505d937ab9
Rajesh <chrajeshkhanna82798@gmail.com>
2017-11-26 12:08:43 +0530

    coin modified

--- coin_flip.js
+++ coin_flip.js
@@ -1,30 +1,50 @@
+var pre_ang = 0;
+
+function flip_coin(){
+	coin = document.getElementById('coin');
+	var angl;
+	coin.style.transitionDuration = '5s';
+	coin.style.transformStyle  = 'preserve-3d';
+	coin.style.perspective = '500px';
+	if (pre_ang <= 0)
+		angl =pre_ang + (180*Math.floor(20+Math.random()*21));
+	else
+		angl =pre_ang - (180*Math.floor(20+Math.random()*2));
+	pre_ang = angl;
+	coin.style.transform = "rotateX("+angl.toString()+"deg)";
+}
+
 var c_card = document.getElementById('coin_card');
-c_card.style.position = 'absolute';
-c_card.style.top = '50px';
-c_card.style.left = '50px';
-c_card.style.height = '550px';
-c_card.style.width = '400px';
-c_card.style.borderRadius = '25px';
-c_card.style.boxShadow= 'inset 0 0 20px rgba(0,0,0,0.2)';
+c_card.id = 'c_card';
 var coin_card = document.createElement('div');
 var text_area = document.createElement('h1');
+var coin = document.createElement('div');
+var head = document.createElement('div');
+var head_text = document.createElement('div');
+head_text.className = 'face_text';
+var tail_text = document.createElement('div');
+tail_text.className = 'face_text';
+head.id = 'head';
+head.className = 'face';
+var tail = document.createElement('div');
+tail.className = 'face';
+tail.id = 'tail';
+coin.setAttribute('id','coin');
 text_area.style.textAlign = "center";
 text_area.innerHTML = 'Flip the coin';
 coin_card.style.width = '400px';
-coin_card.style.height = '400px';
-var coin = document.createElement('div');
+coin_card.style.height = '250px';
 var line = document.createElement('hr');
 line.style.width = '90%';
-coin.style.position = 'absolute';
-coin.style.top = '50px';
-coin.style.left = '50px';
-coin.style.height = '300px';
-coin.style.width = '300px';
-coin.style.align = 'center';
-coin.style.borderRadius = '150px';
-coin.style.backgroundColor = '#222222';
+head_text.innerHTML = 'HEAD';
+tail_text.innerHTML = 'TAIL';
+head.appendChild(head_text);
+tail.appendChild(tail_text);
+coin.appendChild(head);
+coin.appendChild(tail);
 c_card.appendChild(coin_card);
 coin_card.appendChild(coin);
 c_card.appendChild(line);
 c_card.appendChild(text_area);
-
+text_area.addEventListener('click',flip_coin,false);
+text_area.style.cursor = 'pointer';
